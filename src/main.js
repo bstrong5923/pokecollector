99452b6cfcb94b3c1b646db412077eebe5b654e3
bstrong5923 <drizzeerascoll@gmail.com>
2025-04-29 11:37:28 -0700

    fixed the multiple scenes at once problem

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import k from "./kaplayCtx";
 import spinningScene from "./scenes/spinningScene";
 import inventoryScene from "./scenes/inventoryScene";
 
-k.loadBean();
-
-k.scene("test", () => {k.add([k.sprite("bean")])});
-
-k.go("test");
-
 k.loadSprite("boxes", "images/allboxes.png", {
     sliceX: 4,
     sliceY: 2,
@@ -16,7 +10,7 @@ k.loadSprite("wheelborder", "images/wheelborder.png")
 k.loadSprite("bgcolor", "images/bgcolor.png");
 k.loadSprite("spinbutton", "images/spinbutton.png");
 
-k.scene("spinningScene", spinningScene());
-k.scene("inventoryScene", inventoryScene());
+k.scene("spinningScene", () => spinningScene());
+k.scene("inventoryScene", () => inventoryScene());
 
 k.go("inventoryScene");
