d9870e02d176e73091c39a86fc8fa4901f7be25a
bstrong5923 <bstrong5923@student.pps.net>
2025-05-21 18:52:51 -0700

    added weights to the rarities!

--- src/constants.js
+++ src/constants.js
@@ -13,9 +13,10 @@ export class Box {
 }
 
 export class Pack {
-    constructor(name, items) {
+    constructor(name, items, weights) {
         this.name = name;
         this.items = items;
+        this.weights = weights;
     }
     add(x, y) {
         this.sprite = k.add([k.sprite("boxes", { frame: 0 }), k.pos(x, y), k.area()]);
@@ -25,7 +26,19 @@ export class Pack {
         });
     }
     getRandom() {
-        return this.items[Math.floor(Math.random() * this.items.length)].clone();
+        let totalWeight = 0;
+        for (const weight of this.weights) {
+            totalWeight += weight;
+        }
+        const random = Math.random() * totalWeight;
+        let cumulative = 0;
+      
+        for (let i = 0; i < this.items.length; i++) {
+          cumulative += this.weights[i];
+          if (random < cumulative) {
+            return this.items[i].clone();
+          }
+        }
     }
 }
 
@@ -33,9 +46,9 @@ export const inventory = [];
 export const inventoryPage = [0];
 
 export const packs = [
-    new Pack("Pack 1", [new Box(), new Box(), new Box()]),
-    new Pack("Pack 2", [new Box(), new Box(), new Box()]),
-    new Pack("Pack 3", [new Box(), new Box(), new Box()]),
+    new Pack("Pack 1", [new Box(0), new Box(1), new Box(2), new Box(3), new Box(4), new Box(5), new Box(6)], [0.6, 0.25, 0.11, 0.04, 0.012, 0.0035, 0.0006]),
+    new Pack("Pack 2", [new Box(), new Box(), new Box()], [0.1, 0.3, 0.6]),
+    new Pack("Pack 3", [new Box(), new Box(), new Box()], [0.1, 0.3, 0.6]),
     ];
 export const packsPage = [0];
-export const whichPack = [new Pack("0", [])];
+export const whichPack = [new Pack("0", [], [])];
